e2d8090504540d730bd0995c7d64aed7b798f942
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-02-12 22:06:50 +0700

    feat: enhance button styles

--- moviemanagement.client/src/App.tsx
+++ moviemanagement.client/src/App.tsx
@@ -4,6 +4,9 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Promotion from "./pages/promotion/Promotion";
 import PromotionDetail from "./pages/promotion/PromotionDetail";
+import NowShowingMoviesPage from "./pages/movie/NowShowingMoviesPage";
+import UpComingMoviesPage from "./pages/movie/UpComingMoviesPage";
+import MovieDetail from './components/Movie/MovieDetail';
 
 const App = () => {
   return (
@@ -14,6 +17,11 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/promotions" element={<Promotion />} />
         <Route path="/promotions/:id" element={<PromotionDetail />} />
+        <Route path="/movies" >
+          <Route path="now-showing" element={<NowShowingMoviesPage />} />
+          <Route path="up-coming" element={<UpComingMoviesPage />} />
+        </Route>
+        <Route path="/movie/:id" element={<MovieDetail />} />
       </Routes>
     </BrowserRouter>
   );
