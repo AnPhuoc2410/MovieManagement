666985eff255e51e156e46ac331814c6298b8691
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-02-13 22:10:29 +0700

    feat: add favicon

--- moviemanagement.client/src/App.tsx
+++ moviemanagement.client/src/App.tsx
@@ -7,7 +7,6 @@ import PromotionDetail from "./pages/promotion/PromotionDetail";
 import Ticket from "./pages/ticket/Ticket";
 import NowShowingMoviesPage from "./pages/movie/NowShowingMoviesPage";
 import UpComingMoviesPage from "./pages/movie/UpComingMoviesPage";
-import MovieDetail from './components/Movie/MovieDetail';
 
 const App = () => {
   return (
@@ -23,7 +22,6 @@ const App = () => {
             <Route path="now-showing" element={<NowShowingMoviesPage />} />
             <Route path="up-coming" element={<UpComingMoviesPage />} />
           </Route>
-          <Route path="/movie/:id" element={<MovieDetail />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
