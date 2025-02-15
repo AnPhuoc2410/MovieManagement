ab34655ed1c8dac6a02774e3beee275ffea5a8a3
L4azy <hungnqse183854@fpt.edu.vn>
2025-02-16 01:18:32 +0700

    Depression

--- MovieManagement.Server/Models/Configurations/ShowtimeConfiguration.cs
+++ MovieManagement.Server/Models/Configurations/ShowtimeConfiguration.cs
@@ -10,15 +10,17 @@ namespace MovieManagement.Server.Models.Configurations
         {
             builder.ToTable("SHOWTIME");
             builder.HasKey(ct => new { ct.MovieId, ct.StartTime });
+            builder.Property(x => x.MovieId).HasColumnOrder(0);
+            builder.Property(x => x.StartTime).HasColumnOrder(1);
             //builder.Property(x => x.ShowtimeId).HasColumnType("varchar(10)");
             //builder.Property(x => x.ShowtimeId).IsRequired();
             //builder.Property(x => x.ShowtimeId).HasMaxLength(10);
             //builder.Property(x => x.MovieId).HasColumnType("Nvarchar(11)");
             //builder.Property(x => x.RoomId).HasColumnType("varchar(10)");
             //builder.Property(x => x.Price).HasColumnType("decimal(18, 0)");
-            //builder.HasOne(x => x.Movie)
-            //    .WithMany(x => x.Showtimes)
-            //    .HasForeignKey(x => x.MovieId);
+            builder.HasOne(x => x.Movie)
+                .WithMany(x => x.Showtimes)
+                .HasForeignKey(x => x.MovieId);
             //builder.HasOne(x => x.Room)
             //    .WithMany(x => x.Showtimes)
             //    .HasForeignKey(x => x.RoomId);
--- MovieManagement.Server/Models/Entities/Showtime.cs
+++ MovieManagement.Server/Models/Entities/Showtime.cs
@@ -3,17 +3,15 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace MovieManagement.Server.Models.Entities
 {
-    [Table("SHOWTIME")]
+
     public class Showtime
     {
-        [Column(Order = 0)]
+
         public string MovieId { get; set; }
 
-        [Column(Order = 1)]
         public TimeSpan StartTime { get; set; }
 
-        [ForeignKey("MovieId")]
-        public virtual Movie Movie { get; set; }
+        public Movie Movie { get; set; }
 
     }
 }
