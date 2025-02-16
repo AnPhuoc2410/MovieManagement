4963926cac7e0dcb9992ff42bcf75f469f642b3b
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-02-16 11:00:38 +0700

    Half-done DB

--- MovieManagement.Server/Migrations/20250216030551_InitialCreate.cs
+++ MovieManagement.Server/Migrations/20250216030551_InitialCreate.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace MovieManagement.Server.Migrations
 {
     /// <inheritdoc />
-    public partial class asdasd12 : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
