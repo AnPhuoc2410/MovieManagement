35da6c68c35da65e115840ed1b07822ede97a218
NHSang <sangnhse182627@fpt.edu.vn>
2025-02-24 16:58:58 +0700

    hehehe

---
+++ MovieManagement.Server/Extensions/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿//using MovieManagement.Server.Repositories;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace MovieManagement.Server.Extensions
+//{
+//    public class UnitOfWork
+//    {
+//        private readonly SWP391FengShuiKoiSystemContext _context;
+//        private NhanVienRepository _nhanvienRepository;
+
+//        public UnitOfWork() => _context ??= new SWP391FengShuiKoiSystemContext();
+
+
+//        public NhanVienRepository NhanVienRepository
+//        {
+//            get { return _nhanvienRepository ??= new UserRepository(_context); }
+//        }
+
+//        public async Task<int> SaveChangesAsync()
+//        {
+//            return await _context.SaveChangesAsync();
+//        }
+//    }
+//}
