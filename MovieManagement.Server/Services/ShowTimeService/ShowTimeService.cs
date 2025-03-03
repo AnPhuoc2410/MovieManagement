7b7fb53edea702a5df1fedff0d463d0e5987aa68
hocpvse184940 <hocpvse184940@fpt.edu.vn>
2025-03-03 14:16:03 +0700

    Update ShowTime validation

--- MovieManagement.Server/Services/ShowTimeService/ShowTimeService.cs
+++ MovieManagement.Server/Services/ShowTimeService/ShowTimeService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MovieManagement.Server.Data;
+using MovieManagement.Server.Exceptions;
 using MovieManagement.Server.Models.DTOs;
 using MovieManagement.Server.Models.Entities;
 
@@ -12,58 +13,107 @@ namespace MovieManagement.Server.Services.ShowTimeService
 
         public ShowTimeService(IUnitOfWork unitOfWork, IMapper mapper)
         {
-
             _unitOfWork = unitOfWork;
             _mapper = mapper;
-
         }
 
         public async Task<ShowTimeDto> CreateShowTime(ShowTimeDto showtime)
         {
-            var newShowTime = new ShowTime() 
+            var newShowTime = new ShowTime()
             {
                 MovieId = showtime.MovieId,
                 StartTime = showtime.StartTime,
                 RoomId = showtime.RoomId
             };
 
-            return _mapper.Map<ShowTimeDto>(await _unitOfWork.ShowtimeRepository.CreateAsync(newShowTime));
+            try
+            {
+                var createdShowTime = _mapper.Map<ShowTimeDto>(await _unitOfWork.ShowtimeRepository.CreateAsync(newShowTime));
+                return createdShowTime;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("An error occurred while processing into Database", ex);
+            }
         }
 
-        public Task<bool> DeleteShowtime(Guid movieId, Guid roomId)
+        public async Task<bool> DeleteShowtime(Guid movieId, Guid roomId)
         {
-            return _unitOfWork.ShowtimeRepository.DeleteComposeAsync(movieId, roomId);
+            try
+            {
+                var showTime = await _unitOfWork.ShowtimeRepository.GetByComposeIdAsync(movieId, roomId);
+                if (showTime == null)
+                {
+                    throw new NotFoundException("ShowTime does not found!");
+                }
+                return await _unitOfWork.ShowtimeRepository.DeleteComposeAsync(movieId, roomId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Couldn't access into database due to systems error.", ex);
+            }
         }
 
         public async Task<IEnumerable<ShowTimeDto>> GetAllShowtime()
         {
-            var showtimes = await _unitOfWork.ShowtimeRepository.GetAllAsync();
-            return _mapper.Map<List<ShowTimeDto>>(showtimes);
+            try
+            {
+                var showtimes = _mapper.Map<List<ShowTimeDto>>(await _unitOfWork.ShowtimeRepository.GetAllAsync());
+                if (showtimes.Count == 0)
+                {
+                    throw new NotFoundException("ShowTime does not found!");
+                }
+                return showtimes;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Couldn't access into database due to systems error.", ex);
+            }
         }
 
         public async Task<ShowTimeDto> GetShowtime(Guid movieId, Guid roomId)
         {
-            var ticket = await _unitOfWork.ShowtimeRepository.GetByComposeIdAsync(movieId, roomId);
-            //if(ticket == null)
-            //{
-            //    throw new Exception("Show time not found");
-            //}
-            return _mapper.Map<ShowTimeDto>(ticket);  
+            try
+            {
+                var showTime = _mapper.Map<ShowTimeDto>(await _unitOfWork.ShowtimeRepository.GetByComposeIdAsync(movieId, roomId));
+                if (showTime == null)
+                {
+                    throw new NotFoundException("ShowTime does not found!");
+                }
+                return showTime;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Couldn't access into database due to systems error.", ex);
+            }
         }
 
         public async Task<ShowTimeDto> UpdateShowTime(Guid movieId, Guid roomId, ShowTimeDto showtime)
         {
-            var existingShowTime = await _unitOfWork.ShowtimeRepository.GetByComposeIdAsync(movieId, roomId);
-            if (existingShowTime == null)
+            try
             {
-                throw new Exception("Show time not found");
-            }
-            existingShowTime.StartTime = showtime.StartTime;
-            existingShowTime.MovieId = showtime.MovieId;
-            existingShowTime.StartTime = showtime.StartTime;
-            var updatedShowTime = await _unitOfWork.ShowtimeRepository.UpdateAsync(existingShowTime);
-            return _mapper.Map<ShowTimeDto>(updatedShowTime);
-        }
+                if (showtime == null)
+                {
+                    throw new ArgumentNullException("ShowTime", "ShowTime is null");
+                }
 
+                var existingShowTime = await _unitOfWork.ShowtimeRepository.GetByComposeIdAsync(movieId, roomId);
+                if (existingShowTime == null)
+                {
+                    throw new NotFoundException("ShowTime does not found!");
+                }
+
+                existingShowTime.StartTime = showtime.StartTime;
+                existingShowTime.MovieId = showtime.MovieId;
+                existingShowTime.StartTime = showtime.StartTime;
+
+                var updatedShowTime = _mapper.Map<ShowTimeDto>(await _unitOfWork.ShowtimeRepository.UpdateAsync(existingShowTime));
+                return updatedShowTime;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("An error occurred while processing with database.", ex);
+            }
+        }
     }
 }
