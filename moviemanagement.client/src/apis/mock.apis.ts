60752ee3dd3e14b546482f5789517f54dcb3f698
lcaohoanq <hoangclw@gmail.com>
2025-03-03 22:04:29 +0700

    feat: add msw mock api

--- moviemanagement.client/src/apis/mock.apis.ts
+++ moviemanagement.client/src/apis/mock.apis.ts
@@ -1,34 +1,24 @@
 import axios from "axios";
-import { ThanhVien } from "../pages/admin/QuanLyThanhVien/BangThanhVien";
 import { Employee } from "../pages/admin/QuanLyNhanVien/BangNhanVien";
+import { ThanhVien } from "../pages/admin/QuanLyThanhVien/BangThanhVien";
 import { Room } from "../types/room.types";
 
 export const fetchThanhVien = async (): Promise<ThanhVien[]> => {
-  const response = await axios.get<ThanhVien[]>(
-    "https://6512cbd2b8c6ce52b3963937.mockapi.io/api/v1/views",
-  );
+  const response = await axios.get<ThanhVien[]>("/api/thanh-vien");
   return response.data;
 };
 
 export const fetchNhanVien = async (): Promise<Employee[]> => {
-  const response = await axios.get<Employee[]>(
-    "https://6512cbd2b8c6ce52b3963937.mockapi.io/api/v1/views",
-  );
+  const response = await axios.get<Employee[]>("/api/nhan-vien");
   return response.data;
 };
 
 export const fetchRoom = async (): Promise<Room[]> => {
-  const response = await axios.get<Room[]>(
-    "https://run.mocky.io/v3/d96238cd-5721-4b03-81ab-d6da466a048c",
-  );
+  const response = await axios.get<Room[]>("/api/phong-chieu");
   return response.data;
 };
 
 export const fetchRoomDetail = async (id: string): Promise<Room> => {
-  const response = await axios.get<Room>(
-    id === "1"
-      ? "https://run.mocky.io/v3/a46ca746-c33d-4946-92d9-5b44f60293c9"
-      : "notfound",
-  );
+  const response = await axios.get<Room>(`/api/phong-chieu/${id}`);
   return response.data;
 };
