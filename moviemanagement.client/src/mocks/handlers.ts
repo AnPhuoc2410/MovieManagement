7071856146e69a27088189cc28c2ee081e30e90e
lcaohoanq <hoangclw@gmail.com>
2025-03-04 15:02:52 +0700

    feat: add mock data for room detail

--- moviemanagement.client/src/mocks/handlers.ts
+++ moviemanagement.client/src/mocks/handlers.ts
@@ -1,8 +1,14 @@
 import { HttpHandler } from "msw";
-import { handleGetNhanVien, handleGetRoom, handleGetThanhVien } from "./routes";
+import {
+  handleGetNhanVien,
+  handleGetRoom,
+  handleGetRoomDetail,
+  handleGetThanhVien,
+} from "./routes";
 
 export const handlers: HttpHandler[] = [
   handleGetThanhVien(),
   handleGetNhanVien(),
   handleGetRoom(),
+  handleGetRoomDetail("1"),
 ];
