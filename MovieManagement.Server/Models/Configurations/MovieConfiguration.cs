4570fe0aca7c96f7ac3f7589869553075afc671b
Tran Kim Nha <nhatkse182290@fpt.edu.vn>
2025-03-08 00:53:08 +0700

    Modify string length on movie

--- MovieManagement.Server/Models/Configurations/MovieConfiguration.cs
+++ MovieManagement.Server/Models/Configurations/MovieConfiguration.cs
@@ -11,18 +11,18 @@ namespace MovieManagement.Server.Models.Configurations
             builder.ToTable("MOVIE");
             builder.HasKey(x => x.MovieId);
             builder.Property(x => x.MovieId).HasDefaultValueSql("NEWID()");
-            builder.Property(x => x.Name).HasColumnType("nvarchar(50)");
+            builder.Property(x => x.Name).HasColumnType("nvarchar(100)");
             builder.Property(x => x.Name).IsRequired();
-            builder.Property(x => x.Name).HasMaxLength(50);
+            builder.Property(x => x.Name).HasMaxLength(100);
             builder.Property(x => x.Name).IsUnicode(true);
-            builder.Property(x => x.Image).HasColumnType("varchar(50)");
+            builder.Property(x => x.Image).HasColumnType("varchar(250)");
             builder.Property(x => x.Image).IsRequired();
-            builder.Property(x => x.Image).HasMaxLength(50);
-            builder.Property(x => x.Actors).HasColumnType("nvarchar(50)");
-            builder.Property(x => x.Director).HasColumnType("nvarchar(30)");
+            builder.Property(x => x.Image).HasMaxLength(500);
+            builder.Property(x => x.Actors).HasColumnType("nvarchar(250)");
+            builder.Property(x => x.Director).HasColumnType("nvarchar(50)");
             builder.Property(x => x.Rating).HasColumnType("nvarchar(30)");
-            builder.Property(x => x.Trailer).HasColumnType("varchar(50)");
-            builder.Property(x => x.Content).HasColumnType("nvarchar(500)");
+            builder.Property(x => x.Trailer).HasColumnType("varchar(100)");
+            builder.Property(x => x.Content).HasColumnType("nvarchar(1000)");
             builder.Property(x => x.IsDeleted).HasColumnType("bit");
 
 
