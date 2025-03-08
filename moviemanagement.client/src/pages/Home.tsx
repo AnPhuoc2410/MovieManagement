5ec2131c1fa0d48773efff3d3d2d63a2e7351d09
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-03-08 11:55:25 +0700

    feat: implement movie banner slider and enhance homepage layout

--- moviemanagement.client/src/pages/Home.tsx
+++ moviemanagement.client/src/pages/Home.tsx
@@ -1,51 +1,38 @@
 import CloseIcon from "@mui/icons-material/Close";
-import {
-  Box,
-  Button,
-  Container,
-  FormControl,
-  IconButton,
-  InputLabel,
-  MenuItem,
-  Modal,
-  Select,
-  SelectChangeEvent,
-  Typography,
-} from "@mui/material";
-import React, { useState } from "react";
+import { Box, Container, IconButton, Modal, SelectChangeEvent } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
 import ScrollToTop from "../components/common/ScrollToTop";
 import Promotions from "../components/home/Event";
 import Footer from "../components/home/Footer";
 import Header from "../components/home/Header";
 import ListMovies from "../components/home/ListMovies";
 import Membership from "../components/home/Membership";
-import Aurora from "../components/shared/Aurora";
-import { useTranslation } from "react-i18next";
 import QuickBookingComponent from "../components/home/QuickBookingComponent";
-
-const slides = [
-  "https://api-website.cinestar.com.vn/media/MageINIC/bannerslider/1215x365.png",
-  "https://api-website.cinestar.com.vn/media/MageINIC/bannerslider/1215wx365h_4_.jpg",
-  "https://images.squarespace-cdn.com/content/v1/51b3dc8ee4b051b96ceb10de/a528ec50-cc70-46f0-a2e3-31b279594daa/spider-man-across-the-spider-verse-banner-features-an-epic-number-of-spider-people.jpg?format=1500w",
-  "https://th.bing.com/th/id/R.dc64e9a3281832f4d2d87ca943dd4e36?rik=%2fPgUk7wh6wxEQQ&riu=http%3a%2f%2fwww.hidefninja.com%2fwp-content%2fuploads%2f2015%2f01%2finterstellar-banner.jpg&ehk=PYHmoxpd%2f0b3o5xBZIgAI8ojH4Lj7D0Pc37sg8O9mqU%3d&risl=&pid=ImgRaw&r=0",
-  "https://www.bhdstar.vn/wp-content/uploads/2025/02/referenceSchemeHeadOfficeallowPlaceHoldertrueheight1069ldapp.jpg",
-];
+import Aurora from "../components/shared/Aurora";
+import { bannerslider } from "../data/movieBanner.data";
+import MovieBannerSlider from "../components/home/MovieBannerSlider";
 
 const Homepage: React.FC = () => {
   const navigate = useNavigate();
-  const [openModal, setOpenModal] = useState(true);
   const { t } = useTranslation();
+  const [openModal, setOpenModal] = useState(true);
   const [age, setAge] = useState("");
+  const [randomBanner, setRandomBanner] = useState("");
+
+  // Pick a random banner only once when the component mounts
+  useEffect(() => {
+    setRandomBanner(bannerslider[Math.floor(Math.random() * bannerslider.length)]);
+  }, []);
 
   const handleChange = (event: SelectChangeEvent) => {
     setAge(event.target.value as string);
   };
+
   const handleCloseModal = () => {
     setOpenModal(false);
   };
@@ -65,7 +52,7 @@ const Homepage: React.FC = () => {
         overflow: "hidden",
       }}
     >
-      {/* Preview Modal */}
+      {/* Pop-up Modal */}
       <Modal
         open={openModal}
         onClose={handleCloseModal}
@@ -108,21 +95,24 @@ const Homepage: React.FC = () => {
           >
             <CloseIcon />
           </IconButton>
-          <img
-            src={slides[Math.floor(Math.random() * slides.length)]}
-            alt="Movie Banner"
-            style={{
-              width: "100%",
-              height: "auto",
-              maxHeight: "80vh",
-              objectFit: "cover",
-              borderRadius: "8px",
-            }}
-          />
+          {randomBanner && (
+            <img
+              src={randomBanner}
+              alt="Movie Banner"
+              loading="lazy"
+              style={{
+                width: "100%",
+                height: "auto",
+                maxHeight: "80vh",
+                objectFit: "cover",
+                borderRadius: "8px",
+              }}
+            />
+          )}
         </Box>
       </Modal>
 
-      {/* Place Aurora first and style it to cover the entire viewport */}
+      {/* Aurora effect */}
       <Box
         sx={{
           position: "fixed",
@@ -131,21 +121,16 @@ const Homepage: React.FC = () => {
           right: 0,
           bottom: 0,
           zIndex: 0,
-          pointerEvents: "none", // This allows clicks to pass through
+          pointerEvents: "none",
         }}
       >
-        <Aurora
-          colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
-          blend={1.0}
-          amplitude={2.5}
-          speed={1.0}
-        />
+        <Aurora colorStops={["#3A29FF", "#FF94B4", "#FF3232"]} blend={1.0} amplitude={2.5} speed={1.0} />
       </Box>
 
-      {/* Header with adjusted z-index */}
+      {/* Header */}
       <Header />
 
-      {/* Main content container with position relative and z-index */}
+      {/* Main Content */}
       <Container
         sx={{
           paddingTop: { xs: "80px", sm: "90px", md: "100px" },
@@ -155,45 +140,18 @@ const Homepage: React.FC = () => {
         }}
       >
         <Box sx={{ color: "white", mb: 3, mt: 2 }}>
-          <Swiper
-            modules={[Navigation, Pagination, Autoplay]}
-            spaceBetween={50}
-            slidesPerView={1}
-            navigation
-            pagination={{ clickable: false }}
-            autoplay={{ delay: 100000 }}
-            loop={true}
-            style={{
-              width: "100%",
-              height: "auto",
-              maxHeight: { xs: "200px", sm: "300px", md: "400px" },
-            }}
-          >
-            {slides.map((slide, index) => (
-              <SwiperSlide key={index}>
-                <img
-                  src={slide}
-                  alt={`Slide ${index + 1}`}
-                  style={{
-                    width: "100%",
-                    maxHeight: "400px",
-                    objectFit: "initial",
-                    borderRadius: "3px",
-                  }}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+          <MovieBannerSlider />
         </Box>
         <QuickBookingComponent t={t} />
         <ListMovies />
         <Promotions />
       </Container>
+
       {/* Membership */}
       <Membership />
+
       {/* Footer */}
       <ScrollToTop />
-
       <Footer />
     </Box>
   );
