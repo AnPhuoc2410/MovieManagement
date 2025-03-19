5d1b6910fc82940d5526f7b9f93e3e2aba6c2e3e
L4azy <hungnqse183854@fpt.edu.vn>
2025-03-19 15:50:04 +0700

    Update BillRequest.cs

--- MovieManagement.Server/Models/RequestModel/BillRequest.cs
+++ MovieManagement.Server/Models/RequestModel/BillRequest.cs
@@ -2,9 +2,13 @@
 {
     public class BillRequest
     {
-        public int TotalTicket { get; set; }
+        //public int TotalTicket { get; set; }
 
-        public decimal Amount { get; set; }
+        //public decimal Amount { get; set; }
+
+        public List<Guid> Tickets { get; set; } = new List<Guid>();
+
+        public Guid? PromotionId { get; set; }
 
         //public Guid? PromotionId { get; set; }
     }
